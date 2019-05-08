8a3a5a4e2adadc28dbe24c88c4110ec1b4bdcf31
Matthew Denholm <MatthewDenholm@outlook.com>
2019-05-08 16:14:12 +0100

    Added new class to have People objects

---
+++ CodingSolution/CodingSolution/People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingSolution
+{
+    class People
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+
+        People(string nameOne)
+        {
+            lastName = nameOne;
+        }
+
+        People(string nameOne, string nameTwo)
+        {
+            firstName = nameOne;
+            lastName = nameTwo;
+        }
+        People(string nameOne, string nameTwo, string nameThree)
+        {
+            firstName = nameOne;
+            middleName = nameTwo;
+            lastName = nameThree;
+        }
+    }
+}
